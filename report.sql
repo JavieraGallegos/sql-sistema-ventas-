0c8fc4f4bf1d3b87f17eb096220878ae3acda37a
Alexander Hass <gagz.studios@gmail.com>
2026-04-23 10:52:07 -0400

    Update report.sql
    Added Alexander assigned queries

--- report.sql
+++ report.sql
@@ -18,7 +18,8 @@ join detalle_venta dv on dv.id_venta = v.id_venta
 join productos p on p.id_producto = dv.id_producto;
 
 -- 4. Mostrar solo nombre y email de clientes (alexander)
-
+SELECT nombre, email
+FROM clientes;
 
 -- 5. Mostrar solo nombre y precio de productos (javi)
 SELECT nombre, precio
@@ -42,7 +43,9 @@ order by precio desc;
 
 
 -- 9. Clientes ordenados por nombre (alexander)
-
+select nombre 
+from clientes
+order by nombre desc;
 
 -- 10. Detalles de venta con cantidad >= 2 (javi)
 
@@ -69,6 +72,8 @@ left join productos p on p.id_producto = dv.id_producto;
 
 
 -- 14. Precio promedio de productos (alexander)
+SELECT AVG(PRECIO) AS AveragePrice
+FROM PRODUCTOS;
 
 
 -- 15. Suma total de precios de productos (javi)
@@ -95,6 +100,11 @@ JOIN clientes c ON v.id_cliente = c.id_cliente;
     JOIN productos p ON dv.id_producto = p.id_producto;
 
 -- 18. Mostrar nombre del cliente + id de venta + fecha  (alexander)
+SELECT 
+    id_cliente, 
+    id_venta, 
+    fecha
+FROM ventas;
 
 
 -- 19. Mostrar nombre del producto + cantidad vendida + id de venta (javi)
@@ -139,7 +149,17 @@ HAVING COUNT(*) > 1;
 
 
 -- 24. Mostrar las ventas que tienen más de un producto asociado (alexander)
-
+SELECT 
+    v.id_venta, 
+    v.fecha, 
+    v.id_cliente
+FROM ventas v
+JOIN detalle_venta dv ON v.id_venta = dv.id_venta
+GROUP BY 
+    v.id_venta, 
+    v.fecha, 
+    v.id_cliente
+HAVING COUNT(dv.id_producto) > 1;
 
 -- 25. Mostrar clientes cuya suma total de unidades compradas sea mayor a 2 (javi)
 
